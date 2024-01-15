c9321f250f61a6a3b579afbd8f622b8ce32b7f0b
Jay <zerolinuxx@gmail.com>
2024-01-16 00:34:52 +0800

    minor updates

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,18 @@
 import { createApp } from 'vue'
+// import router from './router/router'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
+
+
+// createApp(App)
+//     .use(vuetify)
+//     .use(router)
+//     .mount('#app')
+
 createApp(App)
-  .use(vuetify)
-  .mount('#app')
+    .use(vuetify)
+    .mount('#app')
--- frontend/src/page/HomePage.vue
+++ frontend/src/page/HomePage.vue
@@ -1,118 +1,50 @@
-<style scoped>
-    @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
+<template>    
+    <v-container>
+        <v-row class="text-center">
+            <v-col cols="12">
+                <v-img :src="logo" class="my-3" contain height="200" />
+            </v-col>
+            
+            <v-col class="mb-4">
+                <h1 class="display-2 font-weight-bold mb-3">
+                    Welcome to the Home Page
+                </h1>
+                <h4>HOME</h4>
+            </v-col>
 
-    .rubik {
-        font-family: 'Rubik', sans-serif;
-    }
+            <v-col class="mb-5" cols="12">
+                <h2 class="headline font-weight-bold mb-5">
+                    
+                </h2>
+            </v-col>
 
-    * {
-        font-family: 'Rubik', sans-serif;
-    }
-</style>
-<template>
-<v-container>
-    <v-row class="text-center">
-        <v-col cols="12">
-            <v-img :src="logo" class="my-3" contain height="200" />
-        </v-col>
+            <v-col class="mb-5" cols="12">
 
-        <v-col class="mb-4">
-            <h1 class="display-2 font-weight-bold mb-3">
-                Welcome to the Home Page
-            </h1>
-            <h4>HOME</h4>
-        </v-col>
-
-        <v-col class="mb-5" cols="12">
-            <h2 class="headline font-weight-bold mb-5">
-                What's next?
-            </h2>
-
-            <v-row justify="center">
-                <a v-for="(next, i) in whatsNext" :key="i" :href="next.href" class="subheading mx-3" target="_blank">
-                    {{ next.text }}
-                </a>
-            </v-row>
-        </v-col>
-
-        <v-col class="mb-5" cols="12">
-            <h2 class="headline font-weight-bold mb-5">
-                Important Links
-            </h2>
-
-            <v-row justify="center">
-                <a v-for="(link, i) in importantLinks" :key="i" :href="link.href" class="subheading mx-3" target="_blank">
-                    {{ link.text }}
-                </a>
-            </v-row>
-        </v-col>
-
-        <v-col class="mb-5" cols="12">
-            <h2 class="headline font-weight-bold mb-5">
-                Ecosystem
-            </h2>
-
-            <v-row justify="center">
-                <a v-for="(eco, i) in ecosystem" :key="i" :href="eco.href" class="subheading mx-3" target="_blank">
-                    {{ eco.text }}
-                </a>
-            </v-row>
-        </v-col>
-    </v-row>
-</v-container>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
 import logo from '../assets/logo.svg'
 
 export default {
-    name: 'HelloWorld',
+    name: 'Home',
 
     data: () => ({
-        ecosystem: [{
-                text: 'vuetify-loader',
-                href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
-            },
-            {
-                text: 'github',
-                href: 'https://github.com/vuetifyjs/vuetify/tree/next',
-            },
-            {
-                text: 'awesome-vuetify',
-                href: 'https://github.com/vuetifyjs/awesome-vuetify',
-            },
-        ],
-        importantLinks: [{
-                text: 'Chat',
-                href: 'https://community.vuetifyjs.com',
-            },
-            {
-                text: 'Made with Vuetify',
-                href: 'https://madewithvuejs.com/vuetify',
-            },
-            {
-                text: 'Twitter',
-                href: 'https://twitter.com/vuetifyjs',
-            },
-            {
-                text: 'Articles',
-                href: 'https://medium.com/vuetify',
-            },
-        ],
-        logo,
-        whatsNext: [{
-                text: 'Explore components',
-                href: 'https://vuetifyjs.com',
-            },
-            {
-                text: 'Roadmap',
-                href: 'https://vuetifyjs.com/introduction/roadmap/',
-            },
-            {
-                text: 'Frequently Asked Questions',
-                href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-            },
-        ],
+        logo,   
     }),
 }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
+
+.rubik {
+    font-family: 'Rubik', sans-serif;
+}
+
+* {
+    font-family: 'Rubik', sans-serif;
+}
+</style>
