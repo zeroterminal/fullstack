0b62e509e2235328745b0b28383e5dacded1c87e
Jay <zerolinuxx@gmail.com>
2024-01-18 21:58:10 +0800

    minor updates

--- frontend/src/page/HomePage.vue
+++ frontend/src/page/HomePage.vue
@@ -1,3 +1,19 @@
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
+
+.rubik {
+    font-family: 'Rubik', sans-serif;
+}
+
+* {
+    font-family: 'Rubik', sans-serif;
+}
+
+body {
+    background: #000;
+}
+</style>
+
 <template>    
     <v-container>
         <v-row class="text-center">
@@ -10,41 +26,33 @@
                     Welcome to the Home Page
                 </h1>
                 <h4>HOME</h4>
-            </v-col>
+            </v-col>            
 
             <v-col class="mb-5" cols="12">
                 <h2 class="headline font-weight-bold mb-5">
-                    
+                    <a v-for="(link, i) in links"  :key="i" :href="link.href" class="subheading mx-3" target="_blank">
+                        {{ link.text }}
+                    </a>
                 </h2>
-            </v-col>
-
-            <v-col class="mb-5" cols="12">
-
             </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
+
 import logo from '../assets/logo.svg'
 
 export default {
     name: 'Home',
 
     data: () => ({
-        logo,   
+        logo,
+        links: [
+            { text: "HOME", href: "/"}, 
+            { text: "GALLERY", href: "/gallery"}
+    ]
     }),
 }
 </script>
 
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
-
-.rubik {
-    font-family: 'Rubik', sans-serif;
-}
-
-* {
-    font-family: 'Rubik', sans-serif;
-}
-</style>
